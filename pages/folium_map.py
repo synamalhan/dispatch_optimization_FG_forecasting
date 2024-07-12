f16772f051459b4f77aa7a35d62707827630d2f2
Syna Malhan <synamalhan22@gmail.com>
2024-07-12 11:53:10 +0530

    Update folium_map.py

--- pages/folium_map.py
+++ pages/folium_map.py
@@ -13,12 +13,12 @@ st.page_link("pages/home.py", label="Home", icon=":material/home:")
 
 col1, col2, col3 = st.columns([1, 6, 1])
 with col1:
-    st.image("D:/JSPL/Dispatch_FG/Dispatch/last_point/assets/logo_white.svg", width=150)
+    st.image("../assets/logo_white.svg", width=150)
 with col2:
     st.markdown("<h1 style='text-align: center; font-size: 36px'>Existing Hub Analysis</h1>", unsafe_allow_html=True)
 
 # Load the hub and customer data from the CSV file
-file_path =r'D:\JSPL\Dispatch_FG\Dispatch\last_point\database\database.csv'
+file_path ='../database/database.csv'
 data = load_data(file_path)
 
 # Extract unique hubs with their coordinates
